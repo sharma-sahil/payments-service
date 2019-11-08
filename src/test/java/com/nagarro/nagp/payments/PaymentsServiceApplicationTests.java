80e3602a80c08c49020527239d2699df845b39bb
sahilsharma <sahil.sharma@nagarro.com>
2019-11-08 20:52:43 +0530

    initial commit

---
+++ src/test/java/com/nagarro/nagp/payments/PaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
